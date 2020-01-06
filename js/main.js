cd2ed31b7d8231a4b0000ffa76705874e65046ea
Felippex <55957871+Felippex@users.noreply.github.com>
2020-01-06 21:05:27 +0100

    write slide up and down function in jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const activities = document.querySelectorAll('.in-activities')
+// function for slide down article in activities in pure JS
+/* const activities = document.querySelectorAll('.in-activities')
 console.log(activities);
 
 const articles = document.querySelectorAll('.in-activities article')
@@ -9,4 +10,18 @@ for (let i = 0; i < 5; i++) {
         articles[i].classList.toggle('active')
     }
     activities[i].addEventListener('click', showArticle)
-}
+} */
+
+// the same as above but in jQuery
+$('.in-activities').each(function () {
+    $(this).click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('article', this).slideUp(300);
+
+        } else {
+            $(this).addClass('active');
+            $('article', this).slideDown(300);
+        }
+    })
+})
